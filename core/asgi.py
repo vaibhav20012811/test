cda58f693bfdec3a5e4bb5fb95be1b34ccb78fc9
vaibhav_chamkur <106717595+vaibhav20012811@users.noreply.github.com>
2024-11-04 11:21:29 +0000

    changes

--- core/asgi.py
+++ core/asgi.py
@@ -8,10 +8,10 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
-
-from django.core.asgi import get_asgi_application
 settings_module_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'core.settings')
 print(settings_module_path)
+from django.core.asgi import get_asgi_application
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module_path)
 # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 

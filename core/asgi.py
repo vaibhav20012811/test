5051e812083417d6e10888f140f80297a7683d49
vaibhav-vss <vaibhav.chamkur@vishleshan.net>
2024-11-04 17:05:55 +0530

    wsgi file changes

--- core/asgi.py
+++ core/asgi.py
@@ -8,11 +8,7 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
-settings_module_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'core.settings')
-print(settings_module_path)
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module_path)
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 application = get_asgi_application()
--- core/wsgi.py
+++ core/wsgi.py
@@ -8,12 +8,7 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-settings_module_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'core.settings')
-print(settings_module_path)
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module_path)
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 application = get_wsgi_application()
